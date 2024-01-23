021b535241cdfc51b7ca30eb76bf5b7ca1f06649
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-23 21:36:48 +0300

    webpack server side configuration

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,8 +1,31 @@
 const path = require('path')
 const webpack = require('webpack')
-const  CURRENT_WORKING_DIR = process.cwd()
+const CURRENT_WORKING_DIR = process.cwd()
+const nodeExternals = require('webpack-node-externals')
 
-const config = {}
+const config = {
+    name: "server",
+    entry: [path.join(CURRENT_WORKING_DIR, './server/server.js')],
+    target: "node",
+    output: {
+        path: path.join(CURRENT_WORKING_DIR , '/dist/'),
+        filename: "server.generated.js",
+        publicPath: '/dist/',
+        libraryTarget: "commonjs2"
+    },
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_module/,
+                use: [
+                    'babel-loader'
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = config
 
