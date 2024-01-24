61ec982c502245da079e0f24244be7b7cf6335bd
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-24 10:17:16 +0300

    HelloWorld.js simple frontend component

---
+++ client/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+{/* <h1>Hello World!</h1> */}
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+
+export default hot (module)(HelloWorld)
