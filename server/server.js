818006d58c5cf73bb8c5ac8e929316a3d1104c74
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-24 10:22:08 +0300

    express app initialization

---
+++ server/server.js
@@ -0,0 +1,7 @@
+{/*import the express module inorder to intialize an express app*/}
+
+import express from 'express'
+
+const app = express()
+
+{/* then we will use this express app to build out the rest of the Node server application*/}
