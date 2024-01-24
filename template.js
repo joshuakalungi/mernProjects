96829c60ff2c9981af71a08063a12cad5e781f67
joshuakalungi <joshuakalungi101@gmail.com>
2024-01-24 10:04:37 +0300

    template.js

---
+++ template.js
@@ -0,0 +1,15 @@
+{/* root template file for developing in react */}
+
+export default () => {
+    return `<!doctype html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <title>MERN Skeleton</title>
+            </head>
+            <body>
+                <div id="root"></div>
+                <script type="text/javascript" src="/dist/bundle.js"></script>
+            </body>
+        </html>`
+}
